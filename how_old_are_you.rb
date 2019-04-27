9a4ceeb42ffbef95827cb3665d79241689c07b35
aeisha simmons <ashicks2017@outlook.com>
2019-04-27 23:42:03 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,11 +1,11 @@
 require_relative
-'./current_age_for_birth_year.rb'
+'/.current_age_for_birth_year'
 
 puts "What year were you born?"
 birth_year = gets.to_i 
 
 
-user_age =
-current_age_for_birth_year(birth_year)
+user_age = current_age_for_birth_year(birth_year)
+
 
 puts "You are: " + users_age.to_s + " years old."
